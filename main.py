31a2d4bc0ffcdd92b9018d97aadeecc839633a7b
devmarvis <erobagacollins@gmail.com>
2024-09-06 11:16:28 -0500

    add countdown timer functionality

--- main.py
+++ main.py
@@ -1,24 +1,74 @@
+import math
+import time
+import tkinter
 from tkinter import *
+import textwrap
 
 
 window = Tk()
 window.title("Typing Speed Test")
 window.config(width=600, height=400, padx=32, pady=32)
 
-TIME_LIMIT = 1
+TIME_LIMIT = 0.1
+TEXT_TO_TYPE = ("A text widget manages a multi-line text area. Like the canvas widget, Tk's text widget is an "
+                "immensely flexible and powerful tool that can be used for a wide variety of tasks.")
+wrapped_text = textwrap.fill(TEXT_TO_TYPE, width=50)
 
-# def countdown(count)
+
+def countdown(count):
+    count_mins = math.floor(count / 60)
+    count_secs = int(count % 60)
+
+    if count_secs < 10:
+        count_secs = f"0{count_secs}"
+    canvas.itemconfig(timer_text, text=f"{count_mins}:{count_secs}")
+
+    if count > 0:
+        window.after(1000, countdown, count - 1)
+    else:
+        typing_area.config(state="disabled")
+        text_entered = typing_area.get('1.0', tkinter.END).strip()
+        print(text_entered)
+        try:
+            if text_entered[0]:
+                text_split_arr = text_entered.split(" ")
+                print(f'You typed {len(text_split_arr)} words in 6 seconds.')
+                canvas.itemconfig(notif_text, text=f'You typed {len(text_split_arr)} words in 6 seconds.', )
+        except IndexError:
+            pass
 
 
-canvas = Canvas(width=400, height=150, bg='white', highlightthickness=0)
-canvas.create_text(200, 75, text="Hello, Tkinter!", font=('Arial', 24))
+def start_timer():
+    typing_area.config(state='normal')
+    typing_area.delete('1.0', tkinter.END)
+
+    time.sleep(0.5)
+    count_ = TIME_LIMIT * 60
+    countdown(count_)
+
+
+canvas = Canvas(width=500, height=200, bg='white', highlightthickness=0)
+canvas.create_text(250, 100, text=wrapped_text, font=('Arial', 14))
+timer_text = canvas.create_text(425, 20, text="00:00", font=("Arial", 24, "bold"))
+notif_text = canvas.create_text(125, 20,
+                                text='',
+                                fill='blue',
+                                font=('Comic', 11))
+
 canvas.pack()
 
-typingArea = Text(width=50, height=4, padx=12, pady=8)
-typingArea.pack(pady=12)
+typing_area = Text(width=50, height=4, padx=12, pady=8, state='disabled')
+typing_area.pack(pady=12)
 
-startBtn = Button(text="Start Timer", bg='blue', highlightthickness=0, padx=8, pady=2)
-startBtn.pack()
+start_btn = Button(
+    text="Start Timer",
+    bg='blue',
+    highlightthickness=0,
+    padx=8,
+    pady=2,
+    command=start_timer
+)
+start_btn.pack()
 
 
 window.mainloop()
